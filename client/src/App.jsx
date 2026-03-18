bbd2e4224b1e43d098f2973f61f2a476fc375f6a
JahidulSany <jahidulsanypro@gmail.com>
2026-03-18 21:38:02 +0000

    removed unusual components, updated index.html, and App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Login from './components/Login';
 import Signup from './components/Signup';
-import CourseList from './components/CourseList';
-import CourseDetails from './components/CourseDetails';
-import SubscribedCourses from './components/SubscribedCourses';
-
+import ProjectList from './components/ProjectList';
 import { SessionProvider } from './contexts/SessionContext';
 
 const App = () => {
@@ -15,8 +12,6 @@ const App = () => {
         <Header />
         <Routes>
           <Route path="/" element={<ProjectList />} />
-          <Route path="/project/:id" element={<CourseDetails />} />
-          <Route path="/profile" element={<SubscribedCourses />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="*" element={<h1>Not Found</h1>} />
