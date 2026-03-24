875743775a6d8531d81697ea7315f5bf826fa128
JahidulSany <jahidulsanypro@gmail.com>
2026-03-24 18:03:15 +0000

    Fetch data for project.jsx page

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 
 import questions from '../data/questions.json';
 import Question from '../components/Question';
+import { Navigate } from 'react-router-dom';
+import api from '../api';
 
 const Project = () => {
   const [projectName, setProjectName] = useState('');
@@ -9,35 +11,30 @@ const Project = () => {
   const [objectMethod, setObjectMethod] = useState('');
   const [environmentMethod, setEnvironmentMethod] = useState('');
   const [platform, setPlatform] = useState('');
+  const [message, setMessage] = useState('');
+  const navigate = Navigate();
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const data = {
-      projectName,
-      xrType,
-      objectMethod,
-      environmentMethod,
-      platform,
-    };
-
-    fetch('/api/projects', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    })
-      .then((response) => {
-        response.json();
-        console.log(response);
-      })
-      .then((result) => {
-        console.log('Project created:', result);
-      })
-      .catch((error) => {
-        console.error('Error creating project:', error);
+    try {
+      await api.post('/api/projects', {
+        projectName,
+        xrType,
+        objectMethod,
+        environmentMethod,
+        platform,
       });
+
+      setMessage('Project saved successfully');
+
+      setTimeout(() => {
+        navigate('/profile');
+      }, 1000);
+    } catch (error) {
+      console.error('Error saving project:', error);
+      setMessage('Failed to save project');
+    }
   };
 
   return (
@@ -62,13 +59,30 @@ const Project = () => {
       </div>
 
       <Question q={questions[0]} onSetAnswer={setXrType} />
-      <Question q={questions[1]} onSetAnswer={setObjectMethod} />
-      <Question q={questions[2]} onSetAnswer={setEnvironmentMethod} />
-      <Question q={questions[3]} onSetAnswer={setPlatform} />
+      {xrType && <Question q={questions[1]} onSetAnswer={setObjectMethod} />}
+      {xrType && objectMethod && (
+        <Question q={questions[2]} onSetAnswer={setEnvironmentMethod} />
+      )}
+      {xrType && objectMethod && platform && (
+        <Question q={questions[3]} onSetAnswer={setPlatform} />
+      )}
 
       <div>
-        <button onClick={handleSubmit}>Submit</button>
+        <button
+          onClick={handleSubmit}
+          type="submit"
+          disabled={
+            !projectName ||
+            !xrType ||
+            !objectMethod ||
+            !environmentMethod ||
+            !platform
+          }
+        >
+          Save Project
+        </button>
       </div>
+      {message && <p>{message}</p>}
     </div>
   );
 };
